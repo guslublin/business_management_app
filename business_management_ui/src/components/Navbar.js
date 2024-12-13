8ff27552f2ceb6fc5f8058b2679fbaeb752c9a06
Gustavo Lublin <glublin.desarrollo@jjemp.com>
2024-12-13 12:03:47 -0300

    Navbar corregido, con responsive e ícono de cerrar sesión

--- business_management_ui/src/components/Navbar.js
+++ business_management_ui/src/components/Navbar.js
@@ -1,29 +1,39 @@
 import React, { useContext } from "react";
 import { Layout, Menu, Button } from "antd";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { AuthContext } from "../AuthContext";
+import { LogoutOutlined } from "@ant-design/icons";
 
 const { Header } = Layout;
 
 const Navbar = () => {
   const { logout } = useContext(AuthContext);
+  const location = useLocation();
+
+  // Menú de navegación
+  const items = [
+    { key: "/", label: <Link to="/">Home</Link> },
+    { key: "/usuarios", label: <Link to="/usuarios">Usuarios</Link> },
+    { key: "/configuraciones", label: <Link to="/configuraciones">Configuraciones</Link> },
+  ];
 
   return (
-    <Header style={{ display: "flex", justifyContent: "space-between" }}>
-      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["1"]}>
-        <Menu.Item key="1">
-          <Link to="/">Home</Link>
-        </Menu.Item>
-        <Menu.Item key="2">
-          <Link to="/usuarios">Usuarios</Link>
-        </Menu.Item>
-        <Menu.Item key="3">
-          <Link to="/configuraciones">Configuraciones</Link>
-        </Menu.Item>
-      </Menu>
-      <Button type="danger" onClick={logout}>
-        Cerrar Sesión
-      </Button>
+    <Header style={{ display: "flex", alignItems: "center", padding: "0 20px" }}>
+      {/* Menú de navegación */}
+      <Menu
+        theme="dark"
+        mode="horizontal"
+        selectedKeys={[location.pathname]}
+        items={items}
+        style={{ flex: 1, minWidth: 0 }} // Asegura el ajuste responsive
+      />
+      {/* Botón de logout con ícono */}
+      <Button
+        type="primary"
+        icon={<LogoutOutlined />}
+        onClick={logout}
+        style={{ display: "flex", alignItems: "center", justifyContent: "center" }}
+      />
     </Header>
   );
 };
